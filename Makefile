0dc9a80561c25515eb67d6292f67b4086ada3f15
Mark Cooper <mark.c.cooper@outlook.com>
2024-03-24 11:32:25 -0700

    Re-organize project and use SAM CLI
    Add SAM CLI docs and cfg for local development and testing.
    Moves Terraform resources to modules folder.
    This structure separates dev/test (SAM) from deployment (TF).

--- Makefile
+++ Makefile
@@ -3,27 +3,25 @@ FUNCTION = $(PROJECT)
 
 all: build
 
-.PHONY: build clean import reset
+.PHONY: build import install scan setup
 
-build: clean
-	cd src; pip install -r requirements.txt -t ./
+build:
+	rm -f src/*.zip
 	cd src; zip -X -r $(FUNCTION).zip . -x "*__pycache__*"
 	cp src/$(FUNCTION).zip build/
-	rm src/$(FUNCTION).zip
-
-clean:
-	rm -rf build/*
-	rm -rf src/*/
-	rm -f src/six.py
+	rm src/*.zip
 
 import:
-	python-lambda-local src/tagdb.py event.json -f handler -l src
+	sam local invoke TagDBFunction --docker-network lambda-local --event events/event.json
 
-.PHONY: install
 install:
 	@rbenv install -s
 	@gem install overcommit && overcommit --install && overcommit --sign pre-commit
-	@cd src; pip install -r requirements.txt -t ./
+	@docker network create lambda-local || true
 
-reset:
-	rm ./docker/dynamodb/*.db
+scan:
+	@AWS_ACCESS_KEY_ID=dummy AWS_SECRET_ACCESS_KEY=dummy  \
+		aws dynamodb scan --table-name tagdb --endpoint-url http://localhost:8000
+
+setup:
+	@./create_db.sh
--- create_db.sh
+++ create_db.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
-# AWS_ACCESS_KEY_ID=dummy AWS_SECRET_ACCESS_KEY=dummy ./create_db.sh
-# AWS_PROFILE=myprofile ./create_db.sh https://dynamodb.us-west-2.amazonaws.com
 
-ENDPOINT_URL=${1:-"http://localhost:8000"}
+export AWS_ACCESS_KEY_ID=dummy
+export AWS_SECRET_ACCESS_KEY=dummy
+ENDPOINT_URL=http://localhost:8000
+
+docker compose stop && docker compose rm -f || true
+
+docker compose up --detach
+sleep 1
 
 aws dynamodb create-table --endpoint-url $ENDPOINT_URL \
   --table-name tagdb \
@@ -14,11 +19,3 @@ aws dynamodb create-table --endpoint-url $ENDPOINT_URL \
 aws dynamodb update-table --endpoint-url $ENDPOINT_URL \
   --table-name 'tagdb' \
   --stream-specification StreamEnabled=true,StreamViewType=NEW_IMAGE
-
-# Example: GSI for project specific requirements
-aws dynamodb update-table --endpoint-url $ENDPOINT_URL \
-  --table-name tagdb \
-  --attribute-definitions AttributeName=BuckerImporterId,AttributeType=S \
-  --global-secondary-index-updates \
-    "[{\"Create\":{\"IndexName\": \"BuckerImporterId-index\",\"KeySchema\":[{\"AttributeName\":\"BuckerImporterId\",\"KeyType\":\"HASH\"}], \
-    \"ProvisionedThroughput\": {\"ReadCapacityUnits\": 1, \"WriteCapacityUnits\": 1},\"Projection\":{\"ProjectionType\":\"ALL\"}}}]"
